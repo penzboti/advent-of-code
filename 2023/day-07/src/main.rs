8aa7cd35fc6b94a603ee0db85df92e211919528e
penzboti <penzboti@gmail.com>
2023-12-09 11:34:51 +0100

    day 7 progress, probably will skip this day.

--- 2023/day-07/src/main.rs
+++ 2023/day-07/src/main.rs
@@ -4,6 +4,8 @@ use util::{read_file, split_at_newline};
 // i tought const could only work with string, but i guess its the opposite
 // https://stackoverflow.com/questions/45176403/trying-to-declare-a-string-const-results-in-expected-type-found-my-string
 const STRENGTH: &str = "AKQJT98765432";
+// so we map every letter of the hand, according to their position in string, to abc, and then we use that to sort, and then we somehow push those back, but that is my problem. idk how to push the changes back.
+const ABC: &str = "ABCDEFGHIJKLM";
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 #[derive(Clone)]
@@ -19,7 +21,7 @@ enum Type {
 }
 use crate::Type::*;
 impl Type {
-    fn allTypes() -> [Type; 7] {
+    fn all_types() -> [Type; 7] {
         [FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind, TwoPair, OnePair, HighCard]
     }
 }
@@ -69,16 +71,31 @@ fn part1() -> u32 {
     }
 
     //TODO then we compare the hands, and assign a rank to each one
-    //? just use normal sort, and then work from there?
-    // just so i know
+    // it prob would've worked, if the elements were only enums
+    // but at least i got to know that it works, at the link below
     // https://users.rust-lang.org/t/how-to-sort-vec-enum/58694
-    let mut currank = 1;
-    for h in &mut hands {
-        let h.rank = currank;
-        for h2 in &mut hands {
-            // idk how this gon work yet
+    hands.sort_by(|a, b| a.r#type.cmp(&b.r#type));
+    println!("{:?}", hands);
+    println!("---");
+    let mut index_by_type: Vec<usize> = vec![];
+    for i in 0..hands.len() { if i != hands.len() - 1 {
+        if hands[i+1].r#type == hands[i].r#type {
+            // we only gather the indexes here
+            index_by_type.push(i);
         }
-    }
+        else { 
+            // we compare all the cards here, using the indexes we've gathered
+            // watching this video gave me an idea
+            // https://youtu.be/7zVAz2iMViE?si=2mini-vVeGkvrbar
+            // because finding something that uses the STRENGTH string as a reference, we turn them into chars [a-z] and then sort should sort it in a way that works for us
+            //* idk, my brain is fried
+            //* im close tho, but this sorting is just too much for me atm
+            hands[i+1].rank = hands[i].rank + 1;
+            index_by_type = vec![];
+        }
+    }}
+    println!("{:?}", hands);
+    // dont forget to invert all of the ranks!
 
     let mut sum = 0;
     //TODO then we multiply the bid by the rank
